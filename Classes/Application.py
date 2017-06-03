43a70b48f637b4e582573ea9504bda78f4eceecc
Benjamin Gittelson <benjamingittelson@Benjamins-MacBook-Air.local>
2017-06-02 20:42:17 -0400

    need to fix slate issue--try seeing if you can upgrade to version 0.5.2 in PyCharm

--- Classes/Application.py
+++ Classes/Application.py
@@ -4,20 +4,26 @@ class Application:
         self.idNum = None
         self.text = None
 
-    #get text from a pdf
+    #get text from a pdf using PyPDF2
     def getTextFromPDF(self):
-        # from pyPdf import PdfFileReader
-        # myFile = PdfFileReader(file(self.fileName, "rb"))
         import PyPDF2
         myPDFObj = open(self.fileName, 'rb')
         myReader = PyPDF2.PdfFileReader(myPDFObj)
 
         #extract text from each page
+        pages = []
         for pageNum in range (0, myReader.numPages):
             pageObject = myReader.getPage(pageNum)
             pageText = pageObject.extractText()
-            print pageText
+            pages.append(pageText)
+        self.text = pages
+        #print self.text
 
+    #
+    def getTextWithSlate(self):
+        from pdfminer.pdfdocument import PDFDocument
+        import slate
+        with open(self.fileName) as myFile:
+            myText = slate.PDF(myFile)
+        print myText
 
-        inText = "Ben2"
-        self.text = inText
